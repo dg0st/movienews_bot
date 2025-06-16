8282dc0b93e4518d34907edd0c375fdc337d043b
Adam Lean <adam.lean.92@gmail.com>
2025-06-16 15:41:16 +0800

    Revert "Update config.py"
    This reverts commit 1a663593382314a90162148c59c8a005256c06e1.

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("7350949932:AAE1Vc5AkV3C845gCsduwJjh8iSWY0kBU2w")
-CHANNEL_ID = os.getenv("@plasticcult")  # Например: '@yourchannelname'
-RSS_URL = os.getenv("https://fight-films.info", "https://news.google.com/rss/search?q=action+movies&hl=en&gl=US&ceid=US:en")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHANNEL_ID = os.getenv("CHANNEL_ID")  # Например: '@yourchannelname'
+RSS_URL = os.getenv("RSS_URL", "https://news.google.com/rss/search?q=action+movies&hl=en&gl=US&ceid=US:en")
 KEYWORDS = ["action", "боевик", "thriller", "экшн"]
