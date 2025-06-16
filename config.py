080446e5eacddac88cc082caf71280b6b4794c74
Adam Lean <adam.lean.92@gmail.com>
2025-06-16 15:39:13 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("7350949932:AAE1Vc5AkV3C845gCsduwJjh8iSWY0kBU2w")
-CHANNEL_ID = os.getenv("@plasticcult")  # Например: '@yourchannelname'
-RSS_URL = os.getenv("https://fight-films.info", "https://news.google.com/rss/search?q=action+movies&hl=en&gl=US&ceid=US:en")
+BOT_TOKEN = os.getenv("")
+CHANNEL_ID = os.getenv("")  # Например: '@yourchannelname'
+RSS_URL = os.getenv("")
 KEYWORDS = ["action", "боевик", "thriller", "экшн"]
